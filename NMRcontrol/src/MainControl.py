5fba687835778a85218d48a8fc6d311db621bd85
klein <klein@204.121.146.108>
2017-11-01 12:15:22 -0600

    update of maincontrol currently running but still using lvstart and stop and hearbeat

--- NMRcontrol/src/MainControl.py
+++ NMRcontrol/src/MainControl.py
@@ -11,6 +11,7 @@ import subprocess
 import datetime
 import glob
 import shutil
+import threading # we will run the analyzer in a secodn thread
 
 class MainControl(object):
     """ This modules controls the NMR analyzer and the establishes a handshake between
@@ -23,29 +24,47 @@ class MainControl(object):
     if lvrun and lvheart not updated: exit
     if lvstop and not lvrun: do nothing.
     
+    There are a couple of directories
+    
+    the labview_root directory
+    and beneath it:
+    control  ,  root , backup
+    control has the lvrun,lvstop and lvheart in it
+    root has the converted root files
+    backup has the raw files after it had been converted.
+    
     """
-    def __init__(self,directory, anafile):
+    def __init__(self,directory, anafile, convert_engine_dir):
         
         self.prompt = 'MainControl> '
         self.err = '!!!! err err err !!!!!'
         self.directory = directory # this is the labview storage directory
         self.root_directory = self.directory+'/root'
+        self.control_directory = self.directory+'/control'
         self.backup_directory = self.directory +'/backup' # where raw data get moved once it has been analyzed
+        self.convert_engine_dir = convert_engine_dir
         #check if directory exists:
         if(not os.path.exists(self.directory)):
             message = self.directory+'   directory does not exist'
             self.ErrorMessage(message)
+        if(not os.path.exists(self.control_directory)):
+            message = self.control_directory+'   directory does not exist'
+            self.ErrorMessage(message)
+        
         if( not os.path.exists(self.backup_directory)):
             os.mkdir(self.backup_directory)
         if( not os.path.exists(self.root_directory)):
             os.mkdir(self.root_directory)
         
-        self.run = self.directory+'/lvrun'
-        self.stop = self.directory+'/lvstop'
-        self.heart = self.directory+'/lvheart'
+        self.run = self.control_directory+'/lvrun'
+        self.stop = self.control_directory+'/lvstop'
+        self.heart = self.control_directory+'/lvheart'
         self.last_time = 0 # time stamp of last time change
         
         self.anafile = self.directory+'/'+anafile  # this is filename, where all the analyzed files get stored
+        
+        #go to labview data dir
+        os.chdir(self.directory)
 
     def LastRun(self):
         """ determines the last run analyzed assuming the last line in the file
@@ -60,7 +79,7 @@ class MainControl(object):
                 while f.read(1) != b"\n":   # Until EOL is found...
                     f.seek(-2, os.SEEK_CUR) # ...jump back the read byte plus one more.
                 self.LastAnalyzed = f.readline()         # Read last line.
-                print self.LastAnalyzed
+                print "last file analyzed", self.LastAnalyzed
                 f.close()
         else:
             # create the anafile
@@ -167,11 +186,21 @@ class MainControl(object):
                     # that might be the easiest way, the moment a file is analyzed move it to backup
         # get list of files with extension csv in directory
         f=open(self.anafile,'a') #open file for append
+        
         list_of_files = glob.glob('*.csv')
         for myfile in list_of_files:
             # analyze files
             f.write(myfile) #append to analyzed
-            shutil(self.directory+'/'+myfile , self.backup_directory+'/'+myfile) # move raw file to backup
+            #now strip extension
+            temp=myfile[0:len(myfile)-4]
+
+            arg2 = '-k'+self.directory +'/  -i '+temp
+            command = self.convert_engine_dir +'ReadNMR_short '+arg2 
+            print "***************    ",command
+            os.system(command)
+
+            
+            shutil.move(self.directory+'/'+myfile , self.backup_directory+'/'+myfile) # move raw file to backup
         f.close()
             
         
@@ -185,21 +214,28 @@ class MainControl(object):
         used to det"""
         temp = name[3:len(self.LastAnalyzed)]
         datestring = temp[0:len(temp)-4]
+
         return int(datestring)
     
     
     def GetDate(self): 
-        datestring = self.CreateTime(self.LastAnalyzed)
-        print datestring
+        temp =self.LastAnalyzed[0:3]
+        
+        if(temp != 'TEQ' or temp != 'POL' or temp != 'QCV'):
+            return
+        else:
+            datestring = self.CreateTime(self.LastAnalyzed)
+            print datestring
         # now strip the firts three chracters and the last 4 to just get the labview date
         # now convert to integer
-        mytime = int(datestring)-2082844800  #labview to Unix time alignment
+            mytime = int(datestring)-2082844800  #labview to Unix time alignment
         # convert to unix time
-        print 'last run analyzed ',datetime.datetime.fromtimestamp(mytime).strftime('%Y-%m-%d  @  %H:%M:%SZ')
+            print 'last run analyzed ',datetime.datetime.fromtimestamp(mytime).strftime('%Y-%m-%d  @  %H:%M:%SZ')
         
 
    
         
+
             
                 
             
@@ -217,8 +253,8 @@ class MainControl(object):
 if __name__ == '__main__':
     ana_dir = '/Users/klein/labviewtest'
     ana_name = 'analyzed_files.txt'
-
-    MS = MainControl(ana_dir,ana_name)
+    convert_engine_dir = '/Users/klein/git/NMR_short/ReadNMR_short/Debug/'
+    MS = MainControl(ana_dir,ana_name,convert_engine_dir)
     MS.LastRun()
     MS.GetDate()
     MS.InnerLoop()
